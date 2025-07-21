382b0214711fdf48a31a072bac541345390f728a
Anugna Addula <anugnareddy96@gmail.com>
2025-07-21 21:09:15 +0000

    Add Destination contract and test

---
+++ destination/src/Destination.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./BridgeToken.sol";
+
+contract Destination is AccessControl {
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+	mapping( address => address) public underlying_tokens;
+	mapping( address => address) public wrapped_tokens;
+	address[] public tokens;
+
+	event Creation( address indexed underlying_token, address indexed wrapped_token );
+	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
+	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+
+    constructor( address admin ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(CREATOR_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+    }
+
+	function wrap(address underlying, address recipient, uint256 amount) external onlyRole(WARDEN_ROLE) {
+    BridgeToken token = tokens[underlying];
+    require(address(token) != address(0), "Token not registered");
+
+    token.mint(recipient, amount);
+    emit Wrap(underlying, recipient, amount);
+	}
+
+	function unwrap(address tokenAddress, address recipient, uint256 amount) external {
+    BridgeToken token = BridgeToken(tokenAddress);
+
+    token.burnFrom(msg.sender, amount);
+    emit Unwrap(token.underlying(), recipient, amount);
+	}
+
+	function createToken(address underlying, string memory name, string memory symbol) external onlyRole(CREATOR_ROLE) returns (address) {
+    require(address(tokens[underlying]) == address(0), "Token already created");
+
+    BridgeToken token = new BridgeToken(name, symbol, underlying);
+    tokens[underlying] = token;
+
+    emit Creation(underlying, address(token));
+    return address(token);
+	}
+
+}
+
+
